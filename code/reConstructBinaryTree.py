e35fd66e48db7575383fcc9ab9ff767ee7ee2f01
决心要Coding的Z少女 <36120585+JOINYYz@users.noreply.github.com>
2019-03-21 17:25:22 +0800

    Update reConstructBinaryTree.py

--- code/reConstructBinaryTree.py
+++ code/reConstructBinaryTree.py
@@ -4,7 +4,7 @@ class Solution:
         # write code here
         root = TreeNode(pre[0])
         root_index = tin.index(pre[0])  # 用前序和中序的根节点来确定左右子树范围
-        right_num = len(tin)- root_index -1 #记录根节点右边的节点数
+        right_num = len(tin)- root_index -1 #记录根节点右边的节点数，左边节点数=root_index
         if root_index > 0: # 根节点左边不为空
             root.left = self.reConstructBinaryTree(pre[1:root_index+1],tin[:root_index]) #这里注意self,否则无法递归
         if right_num > 0:  # 根节点右边不为空
