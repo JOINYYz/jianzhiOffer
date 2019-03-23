989f1dfaf508e3e82b30c806fb58c804449cab06
决心要Coding的Z少女 <36120585+JOINYYz@users.noreply.github.com>
2019-03-23 23:03:45 +0800

    Update jumpFloorII.py

--- code/jumpFloorII.py
+++ code/jumpFloorII.py
@@ -4,11 +4,11 @@ class Solution:
         # write code here
         f1 = 1
         f2 = 2
-        sum = f1+f2
+        sum = f1 + f2
         if number < 3:
             return number
         else:
             for i in range(3,number+1):
-                fn = sum + 1 #在跳台阶的基础上，再加上可以一次跳n阶的次数1
-                sum = sum+fn
+                fn = sum + 1 #第一次选择跳m阶后，剩下的是求跳n-m阶的跳法，再加上可以一次跳n阶的次数1
+                sum = sum + fn
             return fn
