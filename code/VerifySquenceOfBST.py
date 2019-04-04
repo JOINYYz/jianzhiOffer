8f0a1080fbd78df1d025a35318955a2fb15f5554
决心要Coding的Z少女 <36120585+JOINYYz@users.noreply.github.com>
2019-04-04 12:45:54 +0800

    Create VerifySquenceOfBST.py

---
+++ code/VerifySquenceOfBST.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def VerifySquenceOfBST(self, sequence):
+        # write code here
+        if sequence == []:
+            return False
+        
+        length = len(sequence)
+        root = sequence[-1]
+        
+        for i in range(length):
+            if sequence[i] > root:
+                break
+        
+        for j in range(i, length): # i仍可用
+            if sequence[j] < root:
+                return False
+            
+        left = True
+        if i > 0:
+            left = self.VerifySquenceOfBST(sequence[:i])
+        right = True
+        if j < length - 1:
+            right = self.VerifySquenceOfBST(sequence[i:length-1])
+            
+        return left and right
+
+                
