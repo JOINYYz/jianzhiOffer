c76a8e044711c8d5ed834151c07d0a109472a944
决心要Coding的Z少女 <36120585+JOINYYz@users.noreply.github.com>
2019-04-17 22:29:00 +0800

    Create TreeDepth2.py

---
+++ code/TreeDepth2.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def TreeDepth(self, pRoot):
+        # write code here
+        if not pRoot:
+            return 0
+        left = self.TreeDepth(pRoot.left)
+        right = self.TreeDepth(pRoot.right)
+        return left+1 if left>right else right+1
+    
